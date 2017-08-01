f896eb1267cfc14671231f71324765c5c37909ab
Ben <beniwhitfield@gmail.com>
2017-08-01 18:12:53 +0100

    Results state management (#3)
    * [Results] fetching hard-coded data to props
    
    * [searchActions] return axios request try different search terms hard coded
    
    * [searchActions.test] implement basic tests
    
    * [App.test] import jest.mock(react-router);
    
    * [index] destructure middlewares, bring in compose from redux
    
    * [packagejson] install proptypes and update redux to 3.1.0

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { searchRepositories } from '../actions/searchActions';
 
 class Results extends Component {
+
+  componentDidMount() {
+    this.props.searchRepositories();
+  }
+
   render() {
+		console.log(this.props)
     return (
       <div>Some Stuff</div>
     );
   }
 }
 
-export default Results;
+const mapDispatchToProps = dispatch => ({
+  searchRepositories: () => {
+    dispatch(searchRepositories());
+  },
+});
+
+const mapStateToProps = state => ({
+  repositories: state.repositories,
+});
+
+Results.propTypes = {
+  searchRepositories: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Results);
