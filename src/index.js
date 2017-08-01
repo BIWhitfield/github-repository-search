e61f7f54a641861fa6bfa4c0c26ef166304b178f
BIWhitfield <beniwhitfield@gmail.com>
2017-08-01 18:09:43 +0100

    [index] destructure middlewares, bring in compose from redux

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
 import Thunk from 'redux-thunk';
 import reducer from './reducers/reducer';
 import App from './App';
 
-// import RouterComponent from './RouterComponent';
 import './css/index.css';
 import './css/font-awesome.css';
 import './css/bulma.css';
 
 const logger = createLogger();
-const store = createStore(reducer, {}, applyMiddleware(Thunk, logger));
+
+const middlewares = [
+  Thunk,
+  logger,
+];
+
+const store = createStore(
+reducer,
+compose(applyMiddleware(...middlewares)));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>, document.getElementById('root'));
+  </Provider>, document.getElementById('root'),
+);
