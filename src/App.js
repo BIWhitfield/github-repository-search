16f906cd963df5398f6b559fc40dcd15ba5776f6
BIWhitfield <beniwhitfield@gmail.com>
2017-08-01 14:33:17 +0100

    [App] replace boilerplate with own dummy components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NavBar from './components/NavBar';
+import SearchForm from './components/SearchForm';
+import Results from './components/Results';
+// import RouterComponent from './RouterComponent';
+import './css/App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = () => (
+  <div className="App">
+    <div className="columns">
+      <div className="column" />
+      <div className="column is-half">
+        <NavBar />
+        <SearchForm />
+        <Results />
       </div>
-    );
-  }
-}
+      <div className="column" />
+    </div>
+  </div>
+);
 
 export default App;
