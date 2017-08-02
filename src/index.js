2b892abc84a736d406ec77f53373303d1586057e
BIWhitfield <beniwhitfield@gmail.com>
2017-08-02 14:20:10 +0100

    [index] remove routing activity to App component

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
+
 import Thunk from 'redux-thunk';
 import reducer from './reducers';
 import App from './App';
@@ -12,9 +11,6 @@ import App from './App';
 import './css/index.css';
 import './css/font-awesome.css';
 import './css/bulma.css';
-
-import SearchForm from './components/SearchForm';
-import Repository from './components/Repository';
 
 const logger = createLogger();
 
@@ -27,16 +23,8 @@ const store = createStore(
 reducer,
 compose(applyMiddleware(...middlewares)));
 
-const history = createBrowserHistory();
-
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
-      <App>
-        <Switch>
-          <Route exact path="/" component={App} />
-        </Switch>
-      </App>
-    </Router>
+    <App />
   </Provider>, document.getElementById('root'),
 );
