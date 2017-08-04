4aa495b94fd6166bc7696a7603da27aecb2e8956
BIWhitfield <beniwhitfield@gmail.com>
2017-08-04 12:05:48 +0100

    [NavBar.test] update render test

--- src/__tests__/NavBar.test.js
+++ src/__tests__/NavBar.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import NavBar from '../components/NavBar';
 
-jest.mock('react-router');
-
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <NavBar />,
-);
-});
+describe('<NavBar />', () => {
+	it('it renders 1 <NavBar /> component', () => {
+		const component = shallow(<NavBar />);
+		expect(component).toHaveLength(1);
+	})
+	it('renders props correctly', () => {
+		const component = shallow(<NavBar name="navbar" />);
+		expect(component.instance().props.name).toBe('navbar');
+	})
+})
